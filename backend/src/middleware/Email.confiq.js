895e4d7113ab3fe6c85d7e4a17ba3602758d8e1c
Rupam-dhote <rupamdhote7@gmail.com>
2025-11-18 19:48:09 +0530

    comit

--- backend/src/middleware/Email.confiq.js
+++ backend/src/middleware/Email.confiq.js
@@ -8,8 +8,8 @@ const pass = process.env.APP_PASSWORD;
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for port 465, false for other ports
+  port: 465,
+  secure: true, // true for port 465, false for other ports
   auth: {
     user: email,
     pass: pass,
